2c21695f47ebae57a6a374943c04fbccc615a799
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-25 14:20:39 +0500

    Initial Commit

---
+++ AnimationFive/AnimationFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationFiveApp.swift
+//  AnimationFive
+//
+//  Created by Abdusamad Mamasoliyev on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
