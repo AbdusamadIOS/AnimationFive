77e183989b378286d179bd4b83954dd7d3d0da21
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-25 14:59:06 +0500

    animation

--- AnimationFive/ContentView.swift
+++ AnimationFive/ContentView.swift
@@ -6,16 +6,53 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let ball = SKSpriteNode(imageNamed: "dodgeBoll")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.7)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let ball = SKSpriteNode(imageNamed: "dodgeBoll")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.7)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+}
 
 struct ContentView: View {
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        
+        return scene
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                SpriteView(scene: scene)
+                    .frame(width: 320, height: 650)
+                    .cornerRadius(20)
+                    .shadow(color: .white, radius: 10)
+            }
         }
-        .padding()
     }
 }
 
